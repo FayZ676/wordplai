2158ca62b4294f0c5a6ddb97b6784c0480f51e3a
Faizi Fifita <94952795+FayZ676@users.noreply.github.com>
2023-12-23 22:39:15 -0800

    basic next auth integration.

--- app/components/rightSidebar.tsx
+++ app/components/rightSidebar.tsx
@@ -2,11 +2,10 @@
 import React, { useState } from "react";
 import Divider from "./divider";
 import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
 
 export default function RightSidebar() {
   const [displaySidebar, setDisplaySidebar] = useState<Boolean>(false);
-  const { data: session } = useSession();
 
   function toggleSidebar() {
     setDisplaySidebar(!displaySidebar);
@@ -31,21 +30,12 @@ export default function RightSidebar() {
             <button className="text-right">Account</button>
             <Divider />
           </div>
-          {session ? (
-            <div className="grid gap-1 mt-auto">
-              <button onClick={() => signOut()} className="text-right">
-                Sign Out
-              </button>
-              <Divider />
-            </div>
-          ) : (
-            <div className="grid gap-1 mt-auto">
-              <button onClick={() => signIn()} className="text-right">
-                Sign In
-              </button>
-              <Divider />
-            </div>
-          )}
+          <div className="grid gap-1 mt-auto">
+            <button onClick={() => signOut()} className="text-right">
+              Sign Out
+            </button>
+            <Divider />
+          </div>
           {/* <div className="grid gap-1 mt-auto">
             <button className="text-right">Sign out</button>
             <Divider />
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,25 @@
-import Image from "next/image";
-import OpenAI from "openai";
-import { useEffect, useState } from "react";
+"use client";
+import { redirect } from "next/navigation";
+import { useSession } from "next-auth/react";
 import Body from "./components/body";
 import LeftSidebar from "./components/leftSidebar";
 import RightSidebar from "./components/rightSidebar";
+import nextAuth, { getServerSession } from "next-auth";
 
-export default async function Home() {
+export default function Home() {
+  const { data: session, status } = useSession();
+
   return (
     <main className="flex-1 grid grid-cols-4 gap-4">
-      <LeftSidebar />
-      <Body />
-      <RightSidebar />
+      {session ? (
+        <>
+          <LeftSidebar />
+          <Body />
+          <RightSidebar />
+        </>
+      ) : (
+        redirect("api/auth/signin")
+      )}
     </main>
   );
 }
